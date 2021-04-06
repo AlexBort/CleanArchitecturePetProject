b6f5c2ec6d44cfcd4644639f376b118c27df0fe2
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-04-07 01:15:09 +0300

    added necessary classes for setting data into recycler

---
+++ app/src/main/java/com/example/cleanarchitecturepetproject/adapters/HeroAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.cleanarchitecturepetproject.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cleanarchitecturepetproject.R
+import com.example.cleanarchitecturepetproject.models.Hero
+import java.util.*
+import kotlin.collections.HashSet
+
+class HeroAdapter : RecyclerView.Adapter<HeroAdapter.ViewHolder>() {
+
+    private val mHeroList: MutableList<Hero> = LinkedList()
+    private val listSet: HashSet<Hero> = HashSet() // unique elements
+
+    fun setDataToCollectionSet(newSet: HashSet<Hero>) {
+        listSet.addAll(newSet)
+        notifyDataSetChanged()
+    }
+
+    fun setData(newHeroes: List<Hero>) {
+        mHeroList.clear()
+        mHeroList.addAll(newHeroes)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) { // it's showed one view-element
+
+        fun bind(hero: Hero) {
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.cell_hero, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(hero = mHeroList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mHeroList.size
+    }
+
+}
---
+++ app/src/main/java/com/example/cleanarchitecturepetproject/models/Hero.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchitecturepetproject.models
+
+data class Hero(
+    val id: Int,
+    val name: String,
+    val title: String,
+    val icon: String/*val localized_name: String*/
+)
