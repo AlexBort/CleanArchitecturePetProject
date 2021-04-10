b89e8d55fd0fbc7b5fe9ea3ae68bf06d6272f92f
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-04-11 01:08:25 +0300

    adding nav_graph.xml; interaction between fragments using nav graph; delete not needed logic from MainActivity

--- app/src/main/java/com/example/cleanarchitecturepetproject/LetterAdapter.kt
+++ app/src/main/java/com/example/cleanarchitecturepetproject/LetterAdapter.kt
@@ -1,19 +1,55 @@
 package com.example.cleanarchitecturepetproject
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
-class LetterAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class LetterAdapter : RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+
+    // Generates a [CharRange] from 'A' to 'Z' and converts it to a list
+    private val list = ('A').rangeTo('Z').toList()
+
+    /**
+     * Provides a reference for the views needed to display items in your list.
+     */
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    /**
+     * Creates new views with R.layout.item_view as its template
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_view, parent, false)
+
+        // Setup custom accessibility delegate to set the text read
+        layout.accessibilityDelegate = WordAdapter
+        return LetterViewHolder(layout)
     }
+
+
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return list.size
     }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+
+        holder.button.setOnClickListener(View.OnClickListener {
+            val action = LetterListFragmentDirections.actionLetterListFragmentToWordListFragment2(letter = holder.button.text.toString())
+            holder.view.findNavController().navigate(action)
+        })
+    }
+
+//    val action = LetterListFragmentDirections.actionLetterListFragmentToWordListFragment(letter = holder.button.text.toString())
+
 
 }
